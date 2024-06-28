d30696f9059149d42ba4cf12cc81ee5f14e2581f
Ryan Hood <ryanhood@ryans-mbp.lan>
2024-06-28 13:49:52 -0500

    Prepare for Heroku deployment using Gunicorn

--- backend/app.py
+++ backend/app.py
@@ -194,6 +194,7 @@ def serve_react_app(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
