18f462aee9cac4ec28a4860c0ec59836f75e9fb6
Ryan Hood <ryanhood@ryans-mbp.lan>
2024-06-28 13:39:26 -0500

    Prepare app for Heroku deployment

--- backend/app.py
+++ backend/app.py
@@ -195,4 +195,5 @@ def serve_react_app(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
